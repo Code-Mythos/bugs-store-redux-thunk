2abad2791f76aadc18debbafca35371218986f69
minervabionic <minerva.bionic@gmail.com>
2022-04-16 23:38:20 +0430

    Action creator functions are added to the project.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import store from "./store";
-import * as actions from "actionTypes";
+import { bugAdded, bugRemoved, bugResolved } from "./action";
 
 const unSubscribe = store.subscribe(() => {
   console.log("Store changed!", store.getState());
 });
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: "Bug 1",
-  },
-});
+store.dispatch(bugAdded("Bug 1"));
 
 unSubscribe();
 
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1,
-  },
-});
+store.dispatch(bugResolved(1));
 
 console.log(store.getState());
