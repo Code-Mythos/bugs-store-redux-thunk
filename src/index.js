bdd4e27eab37d6e6bf0574f55eedf032df7519a3
minervabionic <minerva.bionic@gmail.com>
2022-04-17 18:43:22 +0430

    Another dummy middleware is added / Thunk yet is not practiced.

--- src/index.js
+++ src/index.js
@@ -12,29 +12,7 @@ import { userAdded } from "./store/users";
 
 const store = configureStore();
 
-// const unSubscribe = store.subscribe(() => {
-//   console.log("Store changed!", store.getState());
-// });
-
-store.dispatch(userAdded({ name: "User 1" }));
-store.dispatch(userAdded({ name: "User 2" }));
-
-store.dispatch(projectAdded({ name: "Project 1" }));
-
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
-
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-
-// unSubscribe();
-
-store.dispatch(bugResolved({ id: 1 }));
-
-console.log("State of the store:", store.getState());
-
-const unResolvedBugs = getUnResolvedBugs(store.getState());
-console.log("Unresolved bugs:", unResolvedBugs);
-
-const bugsByUser = getBugsByUser({ userId: 1 })(store.getState());
-console.log("Bugs assigned to user 1:", bugsByUser);
+store.dispatch({
+  type: "error",
+  payload: { message: "Something went wrong!" },
+});
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
+import toast from "./middleware/toast";
 
-// Get pre difined middlewares using getDefaultMiddleware
-// Thunk is one of the default middlewares
-// With thunk middleware, we can return a function instead 
-// of an action in dispatch 
 export default function () {
   return configureStore({
     reducer: reducer,
-    middleware: [...getDefaultMiddleware(), logger],
+    middleware: [...getDefaultMiddleware(), logger, toast],
   });
 }
