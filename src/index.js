42e5f27aedbf43c85c5f07ec9806fe1f9331689f
minervabionic <minerva.bionic@gmail.com>
2022-04-18 13:18:25 +0430

    An API middleware implemented using Thunk and async method to connect to the server.

--- src/index.js
+++ src/index.js
@@ -9,10 +9,14 @@ import {
 } from "./store/bugs";
 import { projectAdded } from "./store/projects";
 import { userAdded } from "./store/users";
+import * as apiActions from "./store/api";
 
 const store = configureStore();
 
-store.dispatch({
-  type: "error",
-  payload: { message: "Something went wrong!" },
-});
+store.dispatch(
+  apiActions.apiCallBegan({
+    url: "/bugs",
+    onSuccess: "bugsReceived",
+    // onError: apiActions.apiCallFailed.type, // Best to be implemented inside the api middleware
+  })
+);
---
+++ src/store/middleware/api.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import * as apiActions from "../api";
+
+// const action = {
+//   type: "apiCallBegan",
+//   payload: {
+//     url: "/bugs",
+//     method: "get",
+//     data: {},
+//     onSuccess: "bugsReceived",
+//     onError: "apiRequestFailed",
+//   },
+// };
+const api = (store) => (next) => async (action) => {
+  const { dispatch } = store;
+  if (action.type !== apiActions.apiCallBegan.type) return next(action);
+
+  next(action);
+
+  const { url, method, data, onSuccess, onError } = action.payload;
+
+  try {
+    const response = await axios.request({
+      baseURL: "http://localhost:9001/api",
+      url,
+      method,
+      data,
+    });
+    // General success
+    dispatch(apiActions.apiCallSuccess(response.data));
+    // Specific success
+    if (onSuccess) {
+      dispatch({
+        type: onSuccess,
+        payload: response.data,
+      });
+    }
+  } catch (error) {
+    // General error handling
+    dispatch(apiActions.apiCallFailed(error));
+    // Specific error handling
+    if (onError) {
+      dispatch({
+        type: onError,
+        payload: error,
+      });
+    }
+  }
+};
+
+export default api;
